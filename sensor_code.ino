cad131b8b0a819b6dddb764a13fc7bf37c2ee943
Rommel Chakravarty <66628385+iamrommelc@users.noreply.github.com>
2022-03-12 17:48:27 +0530

    Update sensor_code.ino

--- sensor_code.ino
+++ sensor_code.ino
@@ -1,7 +1,8 @@
 int flexSensorPin0 = A0;          //analog pin 0 
 int flexSensorPin2 = A1;          //analog pin 1
 int flexSensorPin2 = A2;          //analog pin 2
-
+int flexSensorPin2 = A3;          //analog pin 3
+int flexSensorPin2 = A4;          //analog pin 4
 void setup(){
   Serial.begin(9600);
 }
@@ -21,6 +22,18 @@ void loop()  {
   int flex2 = map(flexSensorReading2, 512, 614,  0, 100);   
   Serial.print(flex2);
   Serial.println(' ');
+  
+  int flexSensorReading3 = analogRead(flexSensorPin3);
+  int flex3 = map(flexSensorReading3, 512, 614,  0, 100);   
+  Serial.print(flex3);
+  Serial.println(' ');
 
+  
+  int flexSensorReading4 = analogRead(flexSensorPin4);
+  int flex4 = map(flexSensorReading4, 512, 614,  0, 100);   
+  Serial.print(flex4);
+  Serial.println(' ');
+  
+  
   delay(250);
 }
